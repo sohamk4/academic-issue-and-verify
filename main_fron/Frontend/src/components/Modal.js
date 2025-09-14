6749a78fd9afa75181568dfb05a914d1dc7ecd7a
sohamk4 <soham56kadam@gmail.com>
2025-09-14 22:44:16 +0530

    Done

--- main_fron/Frontend/src/components/Modal.js
+++ main_fron/Frontend/src/components/Modal.js
@@ -40,8 +40,8 @@ const CertificateEditor = ({ onClose, onSave, studentName }) => {
         {
           headers: {
             "Content-Type": "multipart/form-data",
-            pinata_api_key: "",
-            pinata_secret_api_key: "",
+            pinata_api_key: process.env.PINATA_API_KEY,
+            pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
           },
         }
       );
--- main_fron/Frontend/src/components/issuecertificate.js
+++ main_fron/Frontend/src/components/issuecertificate.js
@@ -175,8 +175,8 @@ const IssueCertificate = () => {
         {
           headers: {
             "Content-Type": "application/json",
-            pinata_api_key: "",
-            pinata_secret_api_key: "",
+            pinata_api_key: process.env.PINATA_API_KEY,
+            pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
           },
         }
       );
